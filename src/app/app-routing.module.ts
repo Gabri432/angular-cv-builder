9da05e3b34cd6df3f2005f74664ae48aebb9e986
Gabri432 <gabrielegatti432@gmail.com>
2024-04-21 13:59:32 +0200

    Defining routes for the components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { EducationComponent } from './routes/education/education.component';
+import { ExperiencesComponent } from './routes/experiences/experiences.component';
+import { SkillsComponent } from './routes/skills/skills.component';
+import { ExtraComponent } from './routes/extra/extra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, title: 'Cv Builder Website'},
+  {path: 'education', component: EducationComponent, title: 'Your degrees and certitications'},
+  {path: 'experiences', component: ExperiencesComponent, title: 'Your Working Experiences'},
+  {path: 'skills', component: SkillsComponent, title: 'Your skills'},
+  {path: 'extra', component: ExtraComponent, title: 'More details about you'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
