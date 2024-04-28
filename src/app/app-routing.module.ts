5c10cd092a86590bd9f5aa5dd4f603e045635e13
Gabri432 <gabrielegatti432@gmail.com>
2024-04-28 18:39:00 +0200

    Simplifying routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
-import { EducationComponent } from './routes/education/education.component';
-import { ExperiencesComponent } from './routes/experiences/experiences.component';
-import { SkillsComponent } from './routes/skills/skills.component';
-import { ExtraComponent } from './routes/extra/extra.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, title: 'Cv Builder Website'},
-  {path: 'education', component: EducationComponent, title: 'Your degrees and certitications'},
-  {path: 'experiences', component: ExperiencesComponent, title: 'Your Working Experiences'},
-  {path: 'skills', component: SkillsComponent, title: 'Your skills'},
-  {path: 'extra', component: ExtraComponent, title: 'More details about you'}
+  {path: '', component: HomeComponent, title: 'Cv Builder Website'}
 ];
 
 @NgModule({
