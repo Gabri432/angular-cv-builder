02d227ec67cd55eabb9977084d9dff87b660d3f3
Gabri432 <gabrielegatti432@gmail.com>
2024-04-30 21:27:59 +0200

    Using a service to decide wheter header and footer should be hidden.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PreviewerService } from './services/previewer.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cv-builder';
+  modePreview = (new PreviewerService).modePreviewOn;
 }
